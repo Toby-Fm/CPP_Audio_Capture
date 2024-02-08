a0f0d7af50d27d9bc1f9db625d7017285ce86061
Toby-fm <mail@toby-wichmann.de>
2024-02-08 22:08:15 +0100

    Hinzufügen von PortAudio-Integration und Geräteinformationen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <stdlib.h>
+#include <portaudio.h>
+
+static void checkError(PaError err) {
+    if (err != paNoError) {
+        std::cerr << "PortAudio error: " << Pa_GetErrorText(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!\n" << std::endl;
+    PaError err;
+    err = Pa_Initialize();
+    checkError(err);
+
+    int numDevices = Pa_GetDeviceCount();
+    std::cout << "\nNumber of devices: " << numDevices << std::endl;
+    if (numDevices < 0) {
+        std::cout << "Error getting device count" << std::endl;
+        exit(EXIT_FAILURE);
+    } else if (numDevices == 0) {
+        std::cout << "No devices found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    const PaDeviceInfo* deviceInfo;
+    for (int i = 0; i < numDevices; i++) {
+        deviceInfo = Pa_GetDeviceInfo(i);
+        std::cout << "\nDevice " << i << ":\n";
+        std::cout << "Name: " << deviceInfo->name << std::endl;
+        std::cout << "Max input channels: " << deviceInfo->maxInputChannels << std::endl;
+        std::cout << "Max output channels: " << deviceInfo->maxOutputChannels << std::endl;
+        std::cout << "Default sample rate: " << deviceInfo->defaultSampleRate << std::endl;
+    }
+
+    err = Pa_Terminate();
+    checkError(err);
+
     return EXIT_SUCCESS;
 }
